e51144d6fe6d0a27118e41070b5abc09c612fb3c
Reza Adelia Widiastuti <71957922+rezaadelia@users.noreply.github.com>
2020-09-27 14:36:09 +0700

    Create variabel_biasa.php

---
+++ variabel_biasa.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Reza Adelia Widiastuti";
+$nim = 2018804493;
+$kelas = "SI SHIFT 2018";
+$Kampus = "STMIK Insan Pembangunan F";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "Nim Mahasiswa : $nim";
+echo "<br>";
+echo "Kelas : $kelas";
+echo "<br>";
+echo "Nama Kampus : $Kampus";
+echo "<br>";
+
+?>
